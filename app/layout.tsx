742a279dcec98735d74ad79692206425d75f8b6c
Rafael Mota <159190187+rafaelmotamakes@users.noreply.github.com>
2025-09-02 19:10:33 -0300

    feat: add facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
+import Script from 'next/script';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 const poppins = Poppins({ 
@@ -29,13 +31,39 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="es_PE">
-      <body className={`${inter.className} ${poppins.variable} font-sans antialiased`}>
-      {children}
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        {children}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2162842897543163');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* Decorative tracking pixel, optimized with Next.js Image */}
+          <Image
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2162842897543163&ev=PageView&noscript=1"
+            alt=""
+            unoptimized
+            priority
+          />
+        </noscript>
       </body>
     </html>
   );
